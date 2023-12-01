42a1940c31d1337136017185b127e7c8b6e1fe3c
Isabella <isabellaotoo25@gmail.com>
2023-12-01 21:06:02 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import { useState } from 'react';
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [newTask, setNewTask] = useState("");
+  const handleChange =(event) => {
+    setNewTask(event.target.value) 
+}
+  
+
   return (
     <div className="App">
       <div className='addTask'>
-        <input></input>
+        <input onChange={handleChange}></input>
         <button>Add Task</button>
+      </div>
+      <div className='lists'>
+
       </div>
       
     </div>
