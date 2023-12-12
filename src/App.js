80a0d032d0eb2559fc4326033f6dad01dc6ac5f6
Isabella <isabellaotoo25@gmail.com>
2023-12-12 10:47:45 +0000

    Add Complete task button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useState } from 'react';
+import { Task } from "./Task"
 
 function App() {
   const [todoList, setTodoList] = useState([]);
@@ -10,7 +11,8 @@ function App() {
   const addTask = () => {
     const task = {
       id: todoList.length === 0 ? 1 :todoList[todoList.length - 1].id + 1,
-      taskName: newTask
+      taskName: newTask,
+      completed: false
     }
     setTodoList([...todoList, task])
   };
@@ -18,6 +20,20 @@ function App() {
   const deleteTask = (id) => {
    
     setTodoList(todoList.filter((task) => task.id !== id));
+  }
+
+  const completeTask = (id) => {
+    setTodoList(
+      todoList.map((task) => {
+        if(task.id === id) {
+          return {...task, completed: true};
+        }
+        else {
+          return task;
+        }
+      }
+    ))
+
   }
   
 
@@ -30,10 +46,12 @@ function App() {
       <div className='lists'>
         {todoList.map((task) =>{
           return(
-            <div>
-              <h1>{task.taskName}</h1>
-              <button onClick={() => deleteTask(task.id)}>X</button>
-            </div>
+            <Task 
+            taskName={task.taskName}
+            id={task.id}
+            completed={task.completed}
+            deleteTask={deleteTask}
+            completeTask={completeTask}/>
           )
 
         })}
